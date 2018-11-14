ec26d6690ea7a2122e2f729adbb02bd9355e1f7d
Parkcheolhyeong <parkcheolhyeong@gamil.com>
2018-11-14 11:06:54 +0900

    Init "Form2"

--- DbProject/Form2.cs
+++ DbProject/Form2.cs
@@ -10,18 +10,16 @@ using System.Windows.Forms;
 
 namespace DbProject
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void loginBtn_Click(object sender, EventArgs e)
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
-            this.Hide();
+            this.Close();
         }
     }
 }
